d0ace2cdd58dd71525f24ec5481122a4af17218d
ayodele <ayodelegodwin0611@gmail.com>
2023-06-19 22:37:01 +0100

    c hello world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a program to print a line with the put function
+ * Return: 0 (when its return then it is a success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle.");
+return (0);
+}
